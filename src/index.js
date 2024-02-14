ef797b5d8bda1126b1b0409cdf1fc6b2e498ddc0
hanminghui <imhanminghui@gmail.com>
2024-02-14 21:57:28 +0800

    use morgan to log

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ const app = express()
 const PORT = "3001";
 const notesRouter = require('./controllers/notes');
 const personsRouter = require('./controllers/persons');
+var morgan = require('morgan');
 
 //middleware 
 app.use(cors());
 app.use(express.json());
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger);
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    '-','reqbody:',
+    JSON.stringify(req.body)
+  ].join(' ')
+}));
 
 //router
 app.get('/', (request, response) => {
