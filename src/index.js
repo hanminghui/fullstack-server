8b74b00a3babd3145b090a0ded7d054b9faf926f
hanminghui <imhanminghui@gmail.com>
2024-02-28 22:50:27 +0800

    add front bulid

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const personsRouter = require('./controllers/persons');
 const screensRouter = require('./controllers/screens');
 var morgan = require('morgan');
 
+app.use(express.static('build'));
+
 //middleware 
 app.use(cors());
 app.use(express.json());
